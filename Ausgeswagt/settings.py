fa40bd3c2d5c2598130c4c45cab559104197c3b3
Cheukting <cheukting.ho@gmail.com>
2025-05-10 18:10:16 +0100

    Disable WhiteNoise storage and configure S3 for media files.
    Commented out the WhiteNoise `STATICFILES_STORAGE` setting, switching to the default Django `StaticFilesStorage` for static files. Updated `STORAGES` configuration to separate static file storage from media files, ensuring proper handling of S3-backed media storage.

--- Ausgeswagt/settings.py
+++ Ausgeswagt/settings.py
@@ -158,7 +158,7 @@ if USE_SPACES:
     # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STORAGES = {
         "default": {
-            "BACKEND": "storages.backends.s3.S3Boto3Storage",
+            "BACKEND": "storages.backends.s3.S3Storage",
         },
         "staticfiles": {
             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
